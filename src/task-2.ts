9cc5d369a36ffff0225aff91e3c4770212042d42
Denis Gulko <denis.gulko.1998@gmail.com>
2026-04-16 12:29:51 +0300

    fix

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,9 +1,9 @@
-interface Products {
-  readonly id: number,
-  title: string,
-  description?: string,
-};
-const product: Products = {
+interface Product {
+  readonly id: number;
+  title: string;
+  description?: string;
+}
+const product: Product = {
   id: 1,
   title: "Tablet",
   description: "Compact and fast",
